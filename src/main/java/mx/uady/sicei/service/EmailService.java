70922ad2b3ae8c39bb4798e54586efb11c3725b1
Ulises Ancona <uliancona@hotmail.com>
2021-01-13 18:15:21 -0600

    Envio de correos final

--- src/main/java/mx/uady/sicei/service/EmailService.java
+++ src/main/java/mx/uady/sicei/service/EmailService.java
@@ -1,6 +1,7 @@
 package mx.uady.sicei.service;
 
 import java.util.Properties;
+import javax.mail.internet.AddressException;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -56,5 +57,24 @@ public class EmailService {
         });
         return session;
     }
+
+    public boolean emailValidator(String email) {
+		boolean isValid = false;
+		try {
+			//
+			// Create InternetAddress object and validated the supplied
+			// address which is this case is an email address.
+			InternetAddress internetAddress = new InternetAddress(email);
+            internetAddress.validate();
+			isValid = true;
+		} catch (AddressException e) {
+			System.out.println("You are in catch block -- Exception Occurred for: " + email);
+		}
+		return isValid;
+	}
+ 
+	public void myLogger(String email, boolean valid) {
+		System.out.println(email + " is " + (valid ? "a" : "not a") + " valid email address\n");
+	}
     
 }
